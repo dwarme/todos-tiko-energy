948cac2cc76d73c1f52346b94545e92bf64d8147
Daouda Warme <dwarme99@gmail.com>
2023-12-13 14:45:57 +0100

    added Layout compoonent

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout({children}: LayoutProps){
+    return (
+        <div>{children}</div>
+    )
+}
